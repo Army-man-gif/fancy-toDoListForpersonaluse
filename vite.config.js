efd2f928a3a02ca586b3c2267179c12809b84f2b
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-07-10 01:28:33 +0100

    '.'

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Courses/WebDevNothingToPro/React_app/Build_files",
+  base: "/fancy-toDoListForpersonaluse",
   plugins: [react()],
   server: {
     open: true,
