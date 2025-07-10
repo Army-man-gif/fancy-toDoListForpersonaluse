71edae087bef1cc93f2ae901722e820c0d8d2a9b
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-07-10 02:11:30 +0100

    '.'

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,6 @@ import { useState, useRef, useEffect } from "react";
 import { nanoid } from "nanoid";
 
 function App() {
-  const myObject = {
-    city: "Madrid",
-    greet() {
-      console.log(`Greetings from ${this.city}`);
-    },
-  };
-
-  myObject.greet(); // Greetings from Madrid
   function usePrevious(value) {
     const ref = useRef();
     useEffect(() => {
