e9e09b8d36310a2b321cbcf8413cc6c8583f76e2
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-07-10 01:12:11 +0100

    sdf

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Three from "./three.jsx";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Three />
+    <App />
   </StrictMode>,
 );
