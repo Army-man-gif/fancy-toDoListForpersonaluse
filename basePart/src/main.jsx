93fcc835ad14e040c2c8fb06545fa688a7703102
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-07-10 17:06:50 +0100

    '.'

--- basePart/src/main.jsx
+++ basePart/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Database from "./testingDatabse.jsx";
-
+import App from "./App.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Database />
+    <App />
   </StrictMode>,
 );
