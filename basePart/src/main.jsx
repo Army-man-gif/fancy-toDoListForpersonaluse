a97e7b22fdbdae52443bb76dd496fcf8cec292b5
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-07-13 02:24:57 +0100

    '.'

--- basePart/src/main.jsx
+++ basePart/src/main.jsx
@@ -1,4 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+//returned to normal
 createRoot(document.getElementById("root")).render(<App />);
