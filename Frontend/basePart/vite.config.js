a440d3e0a4ff89cae50f01cceb0a4340db47e516
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-08-25 18:52:42 +0100

    '.'

--- Frontend/basePart/vite.config.js
+++ Frontend/basePart/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/fancy-toDoListForpersonaluse/docs",
+  base: isProduction ? "/fancy-toDoListForpersonaluse/docs/" : "/",
   plugins: [react()],
-  server: {
-    open: true,
-  },
 });
