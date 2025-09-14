7dcf1ef8d6966f4766deb80cf73e536255bc3e11
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-09-14 12:41:03 +0100

    sfd

--- Backend/ToDoBackend/ToDoBackend/settings.py
+++ Backend/ToDoBackend/ToDoBackend/settings.py
@@ -18,16 +18,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-rarhism!wzi+!9vbv72p5q4$!j0b5k10l75(*syvee23v1n25+"
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS","").split(",")
+CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS","").split(",")
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS","").split(",")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
