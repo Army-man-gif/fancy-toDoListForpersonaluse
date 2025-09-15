ed354643c85d93e09d7306e5bafa5f1591b1feea
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-09-15 12:46:55 +0100

    sdf

--- Backend/ToDoBackend/ToDoBackend/settings.py
+++ Backend/ToDoBackend/ToDoBackend/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "databaseForRecords.middleware.custom_crsf_tokenLogic.CustomCsrfMiddleware",
+    "databaseForRecords.middleware.CustomMiddleware.CustomCsrfMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
