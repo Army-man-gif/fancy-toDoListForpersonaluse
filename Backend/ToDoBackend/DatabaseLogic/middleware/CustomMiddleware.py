33540da10be07dbd071103af28aad246cb984847
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-09-16 12:36:20 +0100

    '.'

--- Backend/ToDoBackend/DatabaseLogic/middleware/CustomMiddleware.py
+++ Backend/ToDoBackend/DatabaseLogic/middleware/CustomMiddleware.py
@@ -6,12 +6,6 @@ class CustomCsrfMiddleware(CsrfViewMiddleware):
     def __init__(self, get_response):
         print("Custom CSRF middleware instantiated!")
         super().__init__(get_response)
-    def _get_token(self, request):
-        print("Custom CSRF Middleware hit!")
-        token = request.META.get('HTTP_X_CSRFTOKEN')
-        if token:
-            return token
-        return super()._get_token(request)
     def process_view(self, request, callback, callback_args, callback_kwargs):
         token_from_header = request.META.get('HTTP_X_CSRFTOKEN')
         if token_from_header:
